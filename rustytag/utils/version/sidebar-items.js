cffc8a3a1e86793334aed6db9f4cec703a24420f
sichang824 <sichang824@users.noreply.github.com>
2025-02-10 11:32:45 +0000

    deploy: eeac00579d6dd728c48e014d8d3916e708923b72

--- rustytag/utils/version/sidebar-items.js
+++ rustytag/utils/version/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["BumpType"],"fn":["get_latest_version","update_cargo_toml","update_package_json","update_pyproject_toml","update_version_to_project"],"struct":["Version"]};
+window.SIDEBAR_ITEMS = {"enum":["BumpType"],"fn":["get_latest_version","update_cargo_toml","update_package_json","update_pyproject_toml","update_rustytag_json","update_version_to_project"],"struct":["Version"]};
